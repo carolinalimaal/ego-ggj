60560a8f069ba3b9e1851d25eafc8a5893aa992f
deLoretoMelo <meloloretomarcelo@gmail.com>
2026-01-31 04:44:01 -0300

    tentativa de mecanicas

---
+++ global-game-jam/masks/masks.gd
@@ -0,0 +1,7 @@
+class_name Masks
+extends Resource
+
+@export var name: String
+@export var image: Texture2D
+@export var colors: Array[Color]
+@export var can_invert_gravity: bool
--- global-game-jam/scenes/player/player.gd
+++ global-game-jam/scenes/player/player.gd
@@ -24,6 +24,14 @@ var facing: int = 1
 @export var gravity_fall: float = 1000
 const jump_multiplier: float = 0.5
 
+# masks variables
+var camaleon_mask: Masks = load("res://masks/resources/camaleon_mask.tres")
+var bat_mask: Masks = load("res://masks/resources/bat_maks.tres")
+var masks: Array[Masks] = []
+var mask_active_index: int = -1
+var can_change_plataform_color: bool = false
+var can_invert_gravity: bool = false
+
 # input variables
 var key_up: bool = false
 var key_down: bool = false
@@ -42,6 +50,9 @@ func _ready() -> void:
 	state_machine.init(self)
 	
 	self.add_to_group("Player")
+	
+	addMask(camaleon_mask)
+	addMask(bat_mask)
 
 func _physics_process(_delta: float) -> void:
 	# getting inputs
@@ -50,6 +61,7 @@ func _physics_process(_delta: float) -> void:
 	# horizontal movement
 	move_and_slide()
 	handleAnimation()
+	handleMaskActivation()
 
 #endregion
 
@@ -83,9 +95,12 @@ func handleLanding() -> void:
 
 func handleJump() -> void:
 	if (key_jump_pressed) and (jumps < max_jumps) and GameManager.can_move:
+		if (can_change_plataform_color):
+			changePlataformColor()
 		jumps += 1
 		sprite.play("jump_animation")
 		state_machine.current_state.transition_to("JumpState")
+		
 		
 
 func horizontalMovement() -> void:
@@ -110,5 +125,44 @@ func handleAnimation() -> void:
 		elif (velocity.y >= 0):
 			sprite.animation = "jump_animation"
 			sprite.frame = 3
+
+func changePlataformColor() -> void:
+	print("Mudou")
+
+func handleMaskActivation() -> void:
+	if (len(masks) > 0):
+		if Input.is_action_just_pressed("mask_1"):
+			if (mask_active_index == -1 or mask_active_index == 1):
+				print("Ativando mascara 0")
+				if mask_active_index == 1:
+					print("mascara 1 desativada")
+				can_change_plataform_color = true
+				can_invert_gravity = false
+				mask_active_index = 0
+			elif mask_active_index == 0:
+				print("Desativando mascara 0")
+				can_change_plataform_color = false
+				mask_active_index = -1
+	if (len(masks) == 2):
+		if Input.is_action_just_pressed("mask_2"):
+			if (mask_active_index == -1 or mask_active_index == 0):
+				print("Ativando mascara 1")
+				if mask_active_index == 0:
+					print("mascara 0 desativada")
+				can_change_plataform_color = false
+				can_invert_gravity = true
+				mask_active_index = 1
+			elif mask_active_index == 1:
+				print("Desativando mascara 1")
+				can_invert_gravity = false
+				mask_active_index = -1
+
+func addMask(mask: Masks):
+	if masks.has(mask):
+		return
+	
+	masks.append(mask)
+	print("Mascara adicioanda: ", mask.name)
+
 
 #endregion
