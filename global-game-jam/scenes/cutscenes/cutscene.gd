7041b6596c3a22d29f770d4539173fe457f59e3f
Carolina de Lima Alves <carol.lima.alves2005@gmail.com>
2026-02-01 22:29:06 -0300

    feat: musicas e sfx cenas e fases

--- global-game-jam/scenes/cutscenes/cutscene.gd
+++ global-game-jam/scenes/cutscenes/cutscene.gd
@@ -1,9 +1,13 @@
 extends Control
 
+@export var cutscene_name: String
 @export var frames_cutscene: Array[Texture2D]
 @export var frame: TextureRect
 @export_file("*.tscn") var next_scene_path: String
+@export var sfx: AudioStreamPlayer
+@onready var sfx_prisma: AudioStreamPlayer = $sfx_prisma
 
+var can_play: bool = true
 var count: int = 0
 var is_transitioning: bool = false
 var transition_duration: float = 0.5
@@ -14,6 +18,14 @@ func _ready() -> void:
 	if frames_cutscene.size() > 0:
 		frame.texture = frames_cutscene[count]
 		_animate_fade_in()
+
+func _process(_delta: float) -> void:
+	if cutscene_name == "Prisma1" and count == 1 and !sfx.playing and can_play:
+		can_play = false
+		sfx.play()
+	if cutscene_name == "Prisma2" and count == 1 and !sfx.playing and can_play:
+		can_play = false
+		sfx.play()
 
 func _unhandled_input(_event: InputEvent) -> void:
 	if is_transitioning:
--- global-game-jam/ui/main_menu/main_menu.gd
+++ global-game-jam/ui/main_menu/main_menu.gd
@@ -4,10 +4,13 @@ extends Control
 
 @onready var start_button: Button = $Menu/Control/Start
 @onready var quit_button: Button = $Menu/Control3/Quit
+@onready var sfx_button: AudioStreamPlayer = $sfx_button
 
 func _ready() -> void:
 	start_button.pressed.connect(_on_start_button_pressed)
 	quit_button.pressed.connect(_on_quit_button_pressed)
+	start_button.focus_entered.connect(_on_focus_entered)
+	quit_button.focus_entered.connect(_on_focus_entered)
 	
 	start_button.grab_focus()
 
@@ -17,3 +20,6 @@ func _on_start_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+func _on_focus_entered() -> void:
+	sfx_button.play()
